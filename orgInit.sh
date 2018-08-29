68aeca46dcf19057c42bd6e93f755dd429de8bd4
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-08-29 08:27:49 -0700

    add a UI component

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,6 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx shane:github:package:install -r lightningSharing -g mshanemc
+sfdx force:package:install --package 04t7F000000CRWdQAO -r -w 20
+04t7F000000CRWdQAO
 sfdx force:source:push
 sfdx force:org:open
