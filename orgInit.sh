4c4868cf49d8726a351bd14c9746cb4defe1847e
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-12-20 09:07:04 -0600

    don't use lightning sharing

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,4 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
-sfdx shane:github:package:install -r lightningSharing -g mshanemc
 sfdx force:package:install --package 04t7F000000CRWdQAO -r -w 20
 sfdx force:source:push
 sfdx force:org:open
