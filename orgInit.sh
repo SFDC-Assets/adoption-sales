0810bfdba147d87793643a4c5f7fcc0e349f7f59
mshanemc <shane.mclaughlin@salesforce.com>
2019-10-10 16:51:42 -0500

    add service cloud/sales stuff to make package deploy

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
-sfdx force:package:install --package 04t7F000000CRWdQAO -r -w 20
+sfdx force:package:install --package 04t7F000000CRWdQAO -r -w 20 -b 20
 sfdx force:source:push
 sfdx force:org:open
