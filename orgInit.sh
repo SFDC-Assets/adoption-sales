b1e73c25dc5136cd56863921cb13d55b082cd847
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-13 00:54:30 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,5 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p adoption -e sales.demo
 sfdx force:package:install --package 04t7F000000CRWdQAO -r -w 20 -b 20
-sfdx force:source:push
-sfdx force:org:open
+sf demoutil user password set -p salesforce1 -g User -l User
+sf project deploy start
+sf org open
